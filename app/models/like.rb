6fdcbfdebdf9b004ff8c8129caec395ac37e0520
AshwinCarvalho999 <ashwincarvalho27@gmail.com>
2023-01-18 16:02:37 +0530

    Processing the data into models

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :update_like_counter
+
+  def update_like_counter
+    post.update(like_counter: post.likes.all.length)
+  end
+end
