353c28f5496d697068609c230297eefe0083ecb5
Joel-100 <joelkugonza@gmail.com>
2023-02-01 14:00:22 +0300

    setup post controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,31 @@
-class PostsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-  end
+# class PostsController < ApplicationController
+#   def index
+#     @user = User.find(params[:user_id])
+#   end
 
-  def show
-    @post = Post.includes(:comments, :likes).find(params[:id])
-  end
+#   def show
+#     @post = Post.includes(:comments, :likes).find(params[:id])
+#   end
 
-  def new
-    @post = Post.new
-  end
+#   def new
+#     @post = Post.new
+#   end
 
-  def create
-    @post = current_user.posts.new(post_params)
+#   def create
+#     @post = current_user.posts.new(post_params)
 
-    if @post.save
-      flash[:success] = 'Your Post has benn created successfully'
-      redirect_to user_post_path(current_user, @post)
-    else
-      flash.now[:error] = 'Error message: Post was not created !'
-      render :new
-    end
-  end
+#     if @post.save
+#       flash[:success] = 'Your Post has benn created successfully'
+#       redirect_to user_post_path(current_user, @post)
+#     else
+#       flash.now[:error] = 'Error message: Post was not created !'
+#       render :new
+#     end
+#   end
 
-  private
+#   private
 
-  def post_params
-    params.require(:post).permit(:title, :text)
-  end
-end
+#   def post_params
+#     params.require(:post).permit(:title, :text)
+#   end
+# end
