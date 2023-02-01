95c79753d2f2c47a530b50d16e0daaf5b84e4a6d
AshwinCarvalho999 <ashwincarvalho27@gmail.com>
2023-02-01 15:37:51 +0530

    Remove current_user method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
-# class ApplicationController < ActionController::Base
-#   protect_from_forgery prepend: true
-#   before_action :configure_permitted_parameters, if: :devise_controller?
-#   before_action :authenticate_user!
+class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-#   protected
+  protected
 
-#   def configure_permitted_parameters
-#     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-#   end
-# end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
