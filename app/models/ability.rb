fcaacf5c09ac150896084c63b4b9a42b45ddc050
Joel-100 <joelkugonza@gmail.com>
2023-02-02 16:29:02 +0300

    Update ability class for user and admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,17 @@
-# class Ability
-#   include CanCan::Ability
+class Ability
+  include CanCan::Ability
 
-#   def initialize(user)
-#     can :read, Post
+  def initialize(user)
+    can :read, Post
 
-#     return unless user.present?
+    return unless user.present?
 
-#     can :manage, Post, user_id: user.id
-#     can %i[create destroy], Comment
+    can :manage, Post, user_id: user.id
+    can %i[create destroy], Comment
 
-#     return unless user.role == 'admin'
+    return unless user.role == 'admin'
 
-#     can :manage, :all
-#   end
+    can :manage, :all
+  end
 
-# end
+end
